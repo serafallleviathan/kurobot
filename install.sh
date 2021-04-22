73be634579a6f71a0aabe44e479aaf1c18df4283
levia <79836946+serafallleviathan@users.noreply.github.com>
2021-04-22 19:22:15 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+apt-get update
+apt-get upgrade
+pkg install wget
+pkg install ffmpeg
+pkg install nodejs
+pkg install tesseract
+npm i -g imgbb-uploader
+npm i -g cwebp
+npm i -g ytdl
+npm i node-tesseract-ocr
+npm i
+npm i got
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm install
+
+echo "[*] SUKSES KAK SILAHKAN KETIK \"node index.js\" Powered by Kevin Apt"
